015d1b702342a29e3d15bc1f580ed03c4b7e473a
igoldin74 <33078752+igoldin74@users.noreply.github.com>
2017-10-28 17:54:59 -0500

    Homework 2 with a question

--- sandbox/src/main/java/com/myfirstprogram/test/MyFirstProgram.java
+++ sandbox/src/main/java/com/myfirstprogram/test/MyFirstProgram.java
@@ -9,22 +9,26 @@ public class MyFirstProgram {
         Point p1 = new Point(10.5, 15.6);
         Point p2 = new Point(20.6, 31.2);
 
-
-        System.out.println("The distance between Point P1 with coordinates: X=" + p1.x + ", Y=" +p1.y+
-                " and Point P2 with coordinates: X=" +p2.x+ ", Y=" +p2.y+ ", equals to " +distance(p1, p2));
+        System.out.println("The distance between Point P1 with coordinates: X=" + p1.x + ", Y=" + p1.y +
+                " and Point P2 with coordinates: X=" + p2.x + ", Y=" + p2.y + ", equals to " + distance(p1, p2));
     }
+        //System.out.println("The distance between Point P1 with coordinates: X=" + p1.x + ", Y=" +p1.y+
+        //        " and Point P2 with coordinates: X=" +p2.x+ ", Y=" +p2.y+ ", equals to " +p.distance());
+        // If I'd use distance() method in my object class, how do I define it here?
 
     public static double distance(Point p1, Point p2) {
 
         {
             int i = 2;
-            double x = p2.x - p1.x;
-            double y = p2.y - p1.y;
+            double x = p1.x - p2.x;
+            double y = p1.y - p2.y;
             double square_x = Math.pow(x, i);
             double square_y = Math.pow(y, i);
             double sum = square_x + square_y;
 
             return Math.sqrt(sum);
         }
+
     }
 }
+
--- sandbox/src/main/java/com/myfirstprogram/test/Point.java
+++ sandbox/src/main/java/com/myfirstprogram/test/Point.java
@@ -10,6 +10,19 @@ public class Point {
         this.y = y;
     }
 
+    public double distance() {
+
+        {
+            int i = 2;
+            double x = this.x - this.x;
+            double y = this.y - this.y;
+            double square_x = Math.pow(x, i);
+            double square_y = Math.pow(y, i);
+            double sum = square_x + square_y;
+
+            return Math.sqrt(sum);
+        }
+    }
 }
 
 
