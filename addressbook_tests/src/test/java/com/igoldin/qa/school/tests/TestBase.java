503cedf4fde98681c9747ac2ea7ea2e9b4c18482
igoldin74 <33078752+igoldin74@users.noreply.github.com>
2017-12-09 16:41:39 -0600

    Added LOGback logger

--- addressbook_tests/src/test/java/com/igoldin/qa/school/tests/TestBase.java
+++ addressbook_tests/src/test/java/com/igoldin/qa/school/tests/TestBase.java
@@ -1,8 +1,15 @@
 package com.igoldin.qa.school.tests;
 
 import com.igoldin.qa.school.appmanager.ApplicationManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
+
+import java.lang.reflect.Method;
+import java.util.Arrays;
 
 import static org.openqa.selenium.remote.BrowserType.FIREFOX;
 
@@ -10,14 +17,25 @@ public class TestBase {
 
     public static ApplicationManager app = new ApplicationManager(FIREFOX);
 
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     @BeforeSuite
     public void setUp() throws Exception {
         app.init();
     }
 
-    @AfterSuite
+    @AfterSuite(alwaysRun = true)
     public void tearDown() {
         app.stop();
     }
 
+    @BeforeMethod
+    public void logTestStart(Method m, Object[] p) {
+        logger.info("Start test " + m.getName() + "with parameters " + Arrays.asList(p));
+    }
+
+    @AfterMethod
+    public void logTestStop(Method m, Object[] p) {
+        logger.info("Stop test " + m.getName() + "with parameters " + Arrays.asList(p));
+    }
 }
